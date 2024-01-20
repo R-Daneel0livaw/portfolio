829245ad35a401c10314418caccf82336440437f
Eric Hine <eric.hine2214@gmail.com>
2024-01-20 14:13:58 -0500

    Add Blog Component.

---
+++ src/components/Blog.tsx
@@ -0,0 +1,24 @@
+export default function Blog() {
+  return (
+    <section id="blog">
+      <header>
+        <h2>Welcome to My Blog Corner!</h2>
+        <p>
+          Dive into the realm of insights, tutorials, and musings on technology,
+          design, and everything in between. This blog is my digital canvas
+          where I paint my thoughts, share my experiences, and provide valuable
+          tips to fellow enthusiasts. 
+          
+          Expect a blend of in-depth articles, step-by-step tutorials, and reflections 
+          on the ever-evolving landscape of Software Development. Whether you're 
+          a seasoned professional or just starting your journey, there's something here 
+          for everyone. 
+          
+          Join me on this exploration of knowledge and creativity. Feel free to
+          comment, share, and let's spark meaningful conversations in this
+          digital space.
+        </p>
+      </header>
+    </section>
+  );
+}
