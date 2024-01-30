0471b83ed5bc1c0a50d2e47313e8f1347fb6988f
Eric Hine <eric.hine2214@gmail.com>
2024-01-29 19:10:47 -0500

    Add nav icons.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import PathConstants from "../../routes/pathConstants";
 import styles from "./Navigation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faUser, faCheck, faDiagramProject, faBook, faMessage } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation() {
   return (
@@ -8,21 +10,39 @@ export default function Navigation() {
       <h2 className={styles.navHeader}>Eric Hine</h2>
       <ul>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faHouse} />
+          </i>
           <Link to={PathConstants.HOME}>Home</Link>
         </li>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faUser} />
+          </i>
           <Link to={PathConstants.ABOUT}>About Me</Link>
         </li>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faCheck} />
+          </i>
           <Link to={PathConstants.SKILLS}>Skills</Link>
         </li>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faDiagramProject} />
+          </i>
           <Link to={PathConstants.PROJECTS}>Projects</Link>
         </li>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faBook} />
+          </i>
           <Link to={PathConstants.BLOG}>Blog</Link>
         </li>
         <li>
+          <i>
+            <FontAwesomeIcon icon={faMessage} />
+          </i>
           <Link to={PathConstants.CONTACT}>Contact</Link>
         </li>
       </ul>
