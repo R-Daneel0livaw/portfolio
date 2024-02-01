30134d59a120bf90255872dd82c0b84a1908bd3f
Eric Hine <eric.hine2214@gmail.com>
2024-02-01 18:44:10 -0500

    Add About Me text.

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,20 +1,88 @@
 export default function AboutMe() {
   return (
     <section id="about">
-      <div>
+      <header>
+      </header>
+
+      <section id="introduction">
+        <h1>Eric Hine</h1>
         <p>
-          Hello, I'm <strong>Eric Hine</strong>. I am a passionate and
-          results-driven <strong>Full-Stack Developer</strong> with a focus on performant solutions.
+          Greetings! I'm a seasoned professional in the tech
+          industry with a passion for continuous learning and a diverse range of
+          interests.
         </p>
+      </section>
+
+      <section id="education-and-professional-journey">
+        <h2>Education and Professional Journey</h2>
         <p>
-          I have a degree in <strong>Computer Science</strong> from The University at Brockport, and have over twelve years
-          experience building Production Applications. 
+          I earned my Bachelor's degree in Computer Science, complemented by a
+          minor in Mathematics, from the esteemed SUNY Brockport. With nearly 15
+          years of professional experience, my journey has been shaped by
+          meaningful contributions to the Financial and Retirement industry.
+          I've had the privilege of working with some of the world's largest
+          retirement providers, gaining valuable insights and expertise along
+          the way.
         </p>
+      </section>
+
+      <section id="tech-enthusiast-and-lifelong-learner">
+        <h2>Tech Enthusiast and Lifelong Learner</h2>
         <p>
-          My mission is to <strong>build, grow, evolve, and learn</strong>. In my free
-          time, I enjoy learning about<strong>History, Philosophy, Logic and Robotics.</strong>.
+          My enthusiasm for technology extends beyond the workplace. I'm not
+          only a tech professional but also a dedicated lifelong learner.
+          Whether it's staying abreast of the latest industry trends or delving
+          into new programming languages, I find joy in the continuous evolution
+          of the tech landscape.
         </p>
-      </div>
+      </section>
+
+      <section id="freelance-web-development">
+        <h2>Freelance Web Development</h2>
+        <p>
+          In addition to my corporate experience, I've ventured into the realm
+          of freelance web development. Building websites has been a rewarding
+          journey, allowing me to leverage my skills and creativity to deliver
+          impactful digital solutions.
+        </p>
+      </section>
+
+      <section id="hobbies">
+        <h2>Hobbies: A Balanced Blend</h2>
+        <p>
+          Outside the world of coding and technology, I have a deep love for
+          literature. You might find me engrossed in a good book or exploring
+          the realms of knowledge through audiobooks. History, Philosophy, Logic, and
+          Robotics are subjects that captivate my curiosity, and I'm always eager
+          to delve into their depths.
+        </p>
+      </section>
+
+      <section id="connect-with-me">
+        <h2>Connect with Me</h2>
+        <p>
+          If you're interested in the details of my professional journey, feel
+          free to explore the rest of my portfolio, as well as my{" "}
+          <a
+            href="your-linkedin-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn profile
+          </a>
+          . It's a comprehensive showcase of my work, experiences, and the
+          professional milestones I've achieved.
+        </p>
+      </section>
+
+      <footer>
+        <p>
+          Thank you for visiting my corner of the web. I invite you to connect,
+          share ideas, and explore the exciting intersections of technology,
+          learning, and life.
+        </p>
+        <p>Best, Eric Hine</p>
+      </footer>
     </section>
   );
 }
