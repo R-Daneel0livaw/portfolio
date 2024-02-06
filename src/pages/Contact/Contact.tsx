7bc3338a07ade87a58f6ea7a748b99e9c587ef36
Eric Hine <eric.hine2214@gmail.com>
2024-02-06 18:59:44 -0500

    Move SocialMedia on Contact Page to right hand-side.

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -6,37 +6,31 @@ import styles from "./Contact.module.css";
 export default function Contact() {
   return (
     <section id="contact" className={styles.contactContainer}>
-      <section id="introduction">
-        <PageHeader
-          title="Contact"
-          description={
-            <p>
-              Get in touch and let me know how I can{" "}
-              <span className={styles.highlight}>help.</span>
-            </p>
-          }
-        />
-      </section>
+      <div className={styles.information}>
+        <section id="introduction">
+          <PageHeader
+            title="Contact"
+            description={
+              <p>
+                Get in touch and let me know how I can{" "}
+                <span className={styles.highlight}>help.</span>
+              </p>
+            }
+          />
+        </section>
 
-      <section className={`${styles.sectionSpacer} ${styles.details}`}>
-        <p className={styles.seeMore}>
-          Thank you for reaching out! Whether it's a project, collaboration, or
-          just a friendly hello, I'm eager to hear from you. No task is too big
-          or small, and your message is my top priority. Let's connect and
-          create something great together!
-        </p>
-      </section>
-
-      <ContactForm />
-
-      {/* <footer className={styles.footerContainer}>
+        <section className={`${styles.sectionSpacer} ${styles.details}`}>
           <p className={styles.seeMore}>
-            Thank you for visiting my corner of the web. I invite you to
-            connect, share ideas, and explore the exciting intersections of
-            technology, learning, and life.
+            Thank you for reaching out! Whether it's a project, collaboration,
+            or just a friendly hello, I'm eager to hear from you. No task is too
+            big or small, and your message is my top priority. Let's connect and
+            create something great together!
           </p>
-          <SignatureWithButton buttonText="Connect" />
-        </footer> */}
+        </section>
+
+        <ContactForm />
+      </div>
+
       <SocialMedia />
     </section>
   );
