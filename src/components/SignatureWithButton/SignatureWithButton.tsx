cc5a9fd700431fd66dc5cc5b5d52faaf0c83f58b
Eric Hine <eric.hine2214@gmail.com>
2024-02-07 18:13:38 -0500

    Route Home and About buttons to Contact page.

--- src/components/SignatureWithButton/SignatureWithButton.tsx
+++ src/components/SignatureWithButton/SignatureWithButton.tsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./SignatureWithButton.module.css";
+import PathConstants from "../../routes/pathConstants";
 
 interface ButtonProps {
   buttonText: string;
 }
 
 export default function SignatureWithButton({ buttonText }: ButtonProps) {
+  const navigate = useNavigate();
+  
   return (
     <>
       <div className={styles.container}>
         <p className={styles.signature}>Eric Hine</p>
-        <button type="submit" className={styles.button}>{buttonText}</button>
+        <button onClick={() => navigate(PathConstants.CONTACT)} type="submit" className={styles.button}>{buttonText}</button>
       </div>
     </>
   );
