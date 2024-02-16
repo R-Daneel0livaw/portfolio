ed58b7d8b160c22d22ceb6d5b49db375be36f65c
Eric Hine <eric.hine2214@gmail.com>
2024-02-15 19:04:52 -0500

    Display lists of skills based on category button being clicked.

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -2,8 +2,25 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Skills.module.css";
+import { useState } from "react";
 
 export default function Skills() {
+  const [activeButtons, setActiveButtons] = useState<string[]>([
+    "programming",
+    "web",
+    "database",
+    "cloud",
+    "devops",
+  ]);
+
+  const handleButtonClick = (target: string) => {
+    if (activeButtons.includes(target)) {
+      setActiveButtons(activeButtons.filter((button) => button !== target));
+    } else {
+      setActiveButtons([...activeButtons, target]);
+    }
+  };
+
   return (
     <section id="skills">
       <PageHeader
@@ -18,100 +35,164 @@ export default function Skills() {
       />
 
       <div className={styles.buttonList}>
-        <button className={`${styles.skillButton} ${styles.selected}`} data-target="programming">
+        <button
+          className={`${styles.skillButton} ${styles.selected}`}
+          data-target="programming"
+          onClick={() => handleButtonClick("programming")}
+        >
           <FontAwesomeIcon icon={faCheck} />
           Programming
         </button>
-        <button className={`${styles.skillButton} ${styles.selected}`} data-target="web">
+        <button
+          className={`${styles.skillButton} ${styles.selected}`}
+          data-target="web"
+          onClick={() => handleButtonClick("web")}
+        >
           <FontAwesomeIcon icon={faCheck} />
           Web
         </button>
-        <button className={`${styles.skillButton} ${styles.selected}`} data-target="database">
+        <button
+          className={`${styles.skillButton} ${styles.selected}`}
+          data-target="database"
+          onClick={() => handleButtonClick("database")}
+        >
           <FontAwesomeIcon icon={faCheck} />
           Database
         </button>
-        <button className={`${styles.skillButton} ${styles.selected}`} data-target="cloud">
+        <button
+          className={`${styles.skillButton} ${styles.selected}`}
+          data-target="cloud"
+          onClick={() => handleButtonClick("cloud")}
+        >
           <FontAwesomeIcon icon={faCheck} />
           Cloud
         </button>
-        <button className={`${styles.skillButton} ${styles.selected}`} data-target="devops">
+        <button
+          className={`${styles.skillButton} ${styles.selected}`}
+          data-target="devops"
+          onClick={() => handleButtonClick("devops")}
+        >
           <FontAwesomeIcon icon={faCheck} />
           DevOps
         </button>
       </div>
 
       <div className={styles.buttonList}>
-        <button className={styles.skillButton} data-target="backend">
+        <button
+          className={styles.skillButton}
+          data-target="backend"
+          onClick={() => handleButtonClick("backend")}
+        >
           Backend
         </button>
-        <button className={styles.skillButton} data-target="emerging">
+        <button
+          className={styles.skillButton}
+          data-target="emerging"
+          onClick={() => handleButtonClick("emerging")}
+        >
           Emerging
         </button>
-        <button className={styles.skillButton} data-target="soft-skills">
+        <button
+          className={styles.skillButton}
+          data-target="soft-skills"
+          onClick={() => handleButtonClick("soft-skills")}
+        >
           Soft Skills
         </button>
       </div>
 
-      <h3>Programming Languages</h3>
-      <ul>
-        <li>Java</li>
-        <li>C/C++/C#</li>
-        <li>Python</li>
-        <li>R</li>
-        <li>JavaScript/TypeScript</li>
-      </ul>
+      {activeButtons.includes("programming") && (
+        <>
+          <h3>Programming Languages</h3>
+          <ul>
+            <li>Java</li>
+            <li>C/C++/C#</li>
+            <li>Python</li>
+            <li>R</li>
+            <li>JavaScript/TypeScript</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Web Technologies</h3>
-      <ul>
-        <li>React</li>
-        <li>Angular</li>
-        <li>Vue</li>
-        <li>HTML5/CSS3</li>
-      </ul>
+      {activeButtons.includes("web") && (
+        <>
+          <h3>Web Technologies</h3>
+          <ul>
+            <li>React</li>
+            <li>Angular</li>
+            <li>Vue</li>
+            <li>HTML5/CSS3</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Backend Technologies</h3>
-      <ul>
-        <li>Spring Boot</li>
-        <li>ASP.NET</li>
-        <li>NodeJS</li>
-      </ul>
+      {activeButtons.includes("backend") && (
+        <>
+          <h3>Backend Technologies</h3>
+          <ul>
+            <li>Spring Boot</li>
+            <li>ASP.NET</li>
+            <li>NodeJS</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Database Management</h3>
-      <ul>
-        <li>SQL (Oracle, PostgreSQL, MySQL)</li>
-        <li>NoSQL (MongoDB, Neo4j, Cassandra)</li>
-      </ul>
+      {activeButtons.includes("database") && (
+        <>
+          <h3>Database Management</h3>
+          <ul>
+            <li>SQL (Oracle, PostgreSQL, MySQL)</li>
+            <li>NoSQL (MongoDB, Neo4j, Cassandra)</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Cloud Platforms</h3>
-      <ul>
-        <li>AWS</li>
-        <li>Azure</li>
-      </ul>
+      {activeButtons.includes("cloud") && (
+        <>
+          <h3>Cloud Platforms</h3>
+          <ul>
+            <li>AWS</li>
+            <li>Azure</li>
+          </ul>
+        </>
+      )}
 
-      <h3>DevOps</h3>
-      <ul>
-        <li>Docker</li>
-        <li>Kubernetes</li>
-        <li>OpenShift</li>
-        <li>Jenkins</li>
-        <li>CI/CD Pipelines</li>
-      </ul>
+      {activeButtons.includes("devops") && (
+        <>
+          <h3>DevOps</h3>
+          <ul>
+            <li>Docker</li>
+            <li>Kubernetes</li>
+            <li>OpenShift</li>
+            <li>Jenkins</li>
+            <li>CI/CD Pipelines</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Emerging Technologies</h3>
-      <ul>
-        <li>Blockchain</li>
-        <li>Machine Learning</li>
-        <li>Internet of Things (IoT)</li>
-      </ul>
+      {activeButtons.includes("emerging") && (
+        <>
+          <h3>Emerging Technologies</h3>
+          <ul>
+            <li>Blockchain</li>
+            <li>Machine Learning</li>
+            <li>Internet of Things (IoT)</li>
+          </ul>
+        </>
+      )}
 
-      <h3>Soft Skills</h3>
-      <ul>
-        <li>Team leadership</li>
-        <li>Mentorship</li>
-        <li>Effective Communication</li>
-        <li>Analytical Thinking</li>
-        <li>Adaptability</li>
-      </ul>
+      {activeButtons.includes("soft-skills") && (
+        <>
+          <h3>Soft Skills</h3>
+          <ul>
+            <li>Team leadership</li>
+            <li>Mentorship</li>
+            <li>Effective Communication</li>
+            <li>Analytical Thinking</li>
+            <li>Adaptability</li>
+          </ul>
+        </>
+      )}
     </section>
   );
 }
