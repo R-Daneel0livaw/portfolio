6ad08720e62bea4fd04d03db43299de474e5e102
Eric Hine <eric.hine2214@gmail.com>
2024-03-22 18:57:07 -0400

    Add temporary blog storage.

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -2,6 +2,60 @@ import styles from "./Blog.module.css";
 
 
 export default function Blog() {
+  type Article = {
+    id: string;
+    topic: string;
+    title: string;
+    description: string;
+    date: string;
+    readTime: string;
+    topics: string[];
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const articles: Article[] = [
+    {
+      id: "post1",
+      topic: "Machine Learning",
+      title: "Mastering Machine Learning Algorithms",
+      description:
+        "Dive into the fascinating world of machine learning algorithms and learn how to implement them from scratch. This comprehensive guide covers a wide range of algorithms, from linear regression to deep learning.",
+      date: "1 February, 2024",
+      readTime: "12 mins",
+      topics: ["tech"],
+    },
+    {
+      id: "post2",
+      topic: "Web Dev",
+      title: "The Power of Progressive Web Apps (PWAs)",
+      description:
+        "Unlock the potential of Progressive Web Apps (PWAs) and revolutionize your web development workflow. Discover how PWAs combine the best of web and mobile experiences, and learn how to build your own.",
+      date: "5 February, 2024",
+      readTime: "15 mins",
+      topics: ["tech"],
+    },
+    {
+      id: "post3",
+      topic: "Cloud",
+      title: "The Future of Cloud Computing: Trends and Innovations",
+      description:
+        "Stay ahead of the curve with insights into the latest trends and innovations in cloud computing. Explore topics such as serverless architecture, multi-cloud strategies, and edge computing, and learn how they are shaping the future of technology.",
+      date: "20 February, 2024",
+      readTime: "10 mins",
+      topics: ["tech"],
+    },
+    {
+      id: "post4",
+      topic: "Architecture",
+      title: "Building Scalable Web Applications with Microservices Architecture",
+      description:
+        "Learn how to design and implement scalable web applications using microservices architecture. Discover best practices for breaking down monolithic applications into smaller, more manageable services, and explore tools and techniques for orchestrating and deploying microservices.",
+      date: "5 March, 2024",
+      readTime: "15 mins",
+      topics: ["tech", "architecture", "tutorial"],
+    },
+  ];
+
   return (
     <section id="blog" className={styles.blogContainer}>
       <header>
