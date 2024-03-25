479a537c832e6272453cc95e9d89207b05f903ab
Eric Hine <eric.hine2214@gmail.com>
2024-03-25 15:47:39 -0400

    Add layout for BlogArticle.

--- src/pages/Blog/BlogArticle/BlogArticle.tsx
+++ src/pages/Blog/BlogArticle/BlogArticle.tsx
@@ -1,14 +1,38 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function BlogArticle() {
   const { id } = useParams(); // Access the article ID from URL params
+  const navigate = useNavigate()
 
   // Fetch/display the article with the given ID
 
   return (
-    <div>
-      <h2>Blog Article {id}</h2>
-      {/* Display the content of the article */}
-    </div>
+    <section id="blog-article">
+      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <header>
+        <h1>Blog Title {id}</h1>
+        <p>
+          Published on: <time dateTime="2024-02-01">February 1, 2024</time>
+        </p>
+        <p>Length: 12 minutes</p>
+        <img src="blog-image.jpg" alt="Blog Image" />
+      </header>
+
+      <section>
+        <h2>Section 1 Title</h2>
+        <p>
+          Section 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. ...
+        </p>
+      </section>
+
+      <section>
+        <h2>Section 2 Title</h2>
+        <p>
+          Section 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. ...
+        </p>
+      </section>
+    </section>
   );
 }
