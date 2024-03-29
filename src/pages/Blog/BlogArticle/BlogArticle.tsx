a7a51d0ea9fbd32c298cca7f62e974d6b81c590b
Eric Hine <eric.hine2214@gmail.com>
2024-03-29 14:07:36 -0400

    Structure BlogArticle into 3 column grid for nav buttons.

--- src/pages/Blog/BlogArticle/BlogArticle.tsx
+++ src/pages/Blog/BlogArticle/BlogArticle.tsx
@@ -2,8 +2,8 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import styles from "./BlogArticle.module.css";
 
 export default function BlogArticle() {
-  const { id } = useParams(); 
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   type Article = {
     id: string;
@@ -13,7 +13,7 @@ export default function BlogArticle() {
     date: string;
     readTime: string;
     topics: string[];
-    comments: number
+    comments: number;
   };
 
   const location = useLocation();
@@ -21,49 +21,60 @@ export default function BlogArticle() {
 
   return (
     <section id="blog-article" className={styles.articleContainer}>
-      <button onClick={() => navigate(-1)} className={styles.previousButton}>
-        &larr; Go back
-      </button>
-      <header>
-        <h1 className={styles.blogTitle}>{state.title} ({id})</h1>
-        <div className={styles.blogDateTime}>
-          <p>
-            <time dateTime="2024-02-01">{state.date}</time>
-          </p>
-          <p>{state.readTime}</p>
-        </div>
-        {/* <img src="blog-image.jpg" alt="Blog Image" /> */}
-      </header>
+      <div className={styles.navContainer}>
+        <button onClick={() => navigate(-1)} className={styles.previousButton}>
+          &larr;
+        </button>
+      </div>
+      <div className={styles.contentContainer}>
+        <header>
+          <h1 className={styles.blogTitle}>
+            {state.title} ({id})
+          </h1>
+          <div className={styles.blogDateTime}>
+            <p>
+              <time dateTime="2024-02-01">{state.date}</time>
+            </p>
+            <p>{state.readTime}</p>
+          </div>
+          {/* <img src="blog-image.jpg" alt="Blog Image" /> */}
+        </header>
 
-      <section>
-        <h2 className={styles.blogSectionTitle}>Section 1 Title</h2>
-        <div className={styles.blogSectionContent}>
-          <p>
-            Section 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. ...
-          </p>
+        <section>
+          <h2 className={styles.blogSectionTitle}>Section 1 Title</h2>
+          <div className={styles.blogSectionContent}>
+            <p>
+              Section 1 Content Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. ...
+            </p>
 
-          <p>
-            Section 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. ...
-          </p>
-        </div>
-      </section>
+            <p>
+              Section 1 Content Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. ...
+            </p>
+          </div>
+        </section>
 
-      <section>
-        <h2 className={styles.blogSectionTitle}>Section 2 Title</h2>
-        <div className={styles.blogSectionContent}>
-          <p>
-            Section 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. ...
-          </p>
+        <section>
+          <h2 className={styles.blogSectionTitle}>Section 2 Title</h2>
+          <div className={styles.blogSectionContent}>
+            <p>
+              Section 2 Content Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. ...
+            </p>
 
-          <p>
-            Section 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. ...
-          </p>
-        </div>
-      </section>
+            <p>
+              Section 2 Content Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. ...
+            </p>
+          </div>
+        </section>
+      </div>
+      <div className={styles.navContainer}>
+        <button onClick={() => navigate(-1)} className={styles.previousButton}>
+          &rarr;
+        </button>
+      </div>
     </section>
   );
 }
