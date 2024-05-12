e582de29fff77571ed96c5ab28addaa2c6077f4e
erich <eric.hine2214@gmail.com>
2024-05-12 09:47:46 -0400

    Add skill button descriptions and adjust sizing of fonts in section.

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,28 +1,13 @@
 import PageHeader from "../../components/PageHeader/PageHeader";
 import {
   faCheck,
-  faArrowsRotate,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Skills.module.css";
-import React, { useState } from "react";
-import CollapsibleSection from "../../components/CollapsibleSection/CollapsibleSection";
+import { useState } from "react";
 
 export default function Skills() {
   const [currentPersona, setCurrentPersona] = useState<string>("Full-Stack Developer");
-  const [activePersonaButton, setActivePersonaButton] = useState("full");
-
-  const [activeSections, setActiveSections] = useState<{
-    [key: string]: number;
-  }>(getDefaultActiveState());
-
-  const [inactiveSections, setInactiveSections] = useState<{
-    [key: string]: number;
-  }>(getDefaultInactiveState());
-
-  const [rotation, setRotation] = useState(0);
-
-  const [expandedActivities, setExpandedActivities] = useState<string[]>([Object.keys(getDefaultActiveState())[0]]);
 
   type Skill = string;
 
@@ -41,7 +26,7 @@ export default function Skills() {
         { shortName: "Programming", longName: "Programming Languages", skills: ["Java", "C/C++/C#", "Python", "R", "Go", "Groovy", "PHP", "JavaScript", "TypeScript"] },
         { shortName: "Backend", longName: "Backend Technologies", skills: ["Spring", ".NET", "NodeJS"] },
         { shortName: "Web", longName: "Web Technologies", skills: ["React", "Angular", "Vue", "Webpack", "Vite", "HTML5/CSS3"] },
-        { shortName: "Database", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
+        { shortName: "Databases", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
         { shortName: "Cloud", longName: "Cloud Platforms", skills: ["AWS", "Azure"] },
         { shortName: "DevOps", longName: "DevOps", skills: ["Docker", "Kubernetes", "OpenShift", "Jenkins", "CI/CD Pipelines", "Git"] },
       ],
@@ -50,7 +35,7 @@ export default function Skills() {
       "Frontend Developer",
       [
         { shortName: "Web", longName: "Web Technologies", skills: ["React", "Angular", "Vue", "Webpack", "Vite", "HTML5/CSS3"] },
-        { shortName: "Database", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
+        { shortName: "Databases", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
         { shortName: "UI/UX", longName: "UI/UX", skills: ["Figma", "Sketch", "Adobe Photoshop", "Tailwind", "Bootstrap", "Material UI", "Accessibility"] },
         { shortName: "Cloud", longName: "Cloud Platforms", skills: ["AWS", "Azure"] },
       ],
@@ -66,10 +51,10 @@ export default function Skills() {
       "Data Analyst",
       [
         { shortName: "Programming", longName: "Programming Languages", skills: ["Java", "C/C++/C#", "Python", "R", "Go", "Groovy", "PHP", "JavaScript", "TypeScript"] },
+        { shortName: "Machine Learning", longName: "Machine Learning", skills: ["Scikit-learn", "Amazon SageMaker", "Tensor-Flow", "Keras", "CARET"] },
         { shortName: "Data Visualization", longName: "Data Visualization", skills: ["Tableau", "Power BI", "Seaborn"] },
         { shortName: "Big Data", longName: "Big Data Technologies", skills: ["Hadoop", "Spark"] },
-        { shortName: "Machine Learning", longName: "Machine Learning", skills: ["Scikit-learn", "Amazon SageMaker", "Tensor-Flow", "Keras", "CARET"] },
-        { shortName: "Database", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
+        { shortName: "Databases", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
       ],
     ],
     [
@@ -85,12 +70,12 @@ export default function Skills() {
         { shortName: "Programming", longName: "Programming Languages", skills: ["Java", "C/C++/C#", "Python", "R", "Go", "Groovy", "PHP", "JavaScript", "TypeScript"] },
         { shortName: "Backend", longName: "Backend Technologies", skills: ["Spring", ".NET", "NodeJS"] },
         { shortName: "Web", longName: "Web Technologies", skills: ["React", "Angular", "Vue", "Webpack", "Vite", "HTML5/CSS3"] },
-        { shortName: "Database", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
+        { shortName: "Databases", longName: "Database Management", skills: ["SQL", "NoSQL", "Vector", "Oracle", "PostgreSQL", "MySQL", "MongoDB", "Neo4j", "Cassandra"] },
         { shortName: "Cloud", longName: "Cloud Platforms", skills: ["AWS", "Azure"] },
         { shortName: "DevOps", longName: "DevOps", skills: ["Docker", "Kubernetes", "OpenShift", "Jenkins", "CI/CD Pipelines", "Git"] },
+        { shortName: "Machine Learning", longName: "Machine Learning", skills: ["Scikit-learn", "Amazon SageMaker", "Tensor-Flow", "Keras", "CARET"] },
         { shortName: "Data Visualization", longName: "Data Visualization", skills: ["Tableau", "Power BI", "Seaborn"] },
         { shortName: "Big Data", longName: "Big Data Technologies", skills: ["Hadoop", "Spark"] },
-        { shortName: "Machine Learning", longName: "Machine Learning", skills: ["Scikit-learn", "Amazon SageMaker", "Tensor-Flow", "Keras", "CARET"] },
         { shortName: "UI/UX", longName: "UI/UX", skills: ["Figma", "Sketch", "Adobe Photoshop", "Tailwind", "Bootstrap", "Material UI", "Accessibility"] },
         { shortName: "Leadership", longName: "Leadership and Commnuication", skills: ["Team Leadership", "Mentorship", "Effective Commnuication", "Analytical Thinking"] },
         { shortName: "Soft Skills", longName: "Soft Skills", skills: ["Collaboration", "Time Management", "Adaptability", "Resource Planning"] },
@@ -98,81 +83,14 @@ export default function Skills() {
     ],
   ]);
 
-
-  const handlePersonaButtonClick = (
-    target: string,
-    buttonsToEnable: string[]
-  ) => {
-    const allActivities = [
-      ...Object.keys(getDefaultActiveState()),
-      ...Object.keys(getDefaultInactiveState()),
-    ].filter((i) => !buttonsToEnable.includes(i));
-
-    const include: { [key: string]: number } = buttonsToEnable.reduce<{
-      [key: string]: number;
-    }>((acc, currentValue, index) => {
-      acc[currentValue] = index;
-      return acc;
-    }, {});
-
-    const exclude: { [key: string]: number } = allActivities.reduce<{
-      [key: string]: number;
-    }>((acc, currentValue, index) => {
-      acc[currentValue] = index;
-      return acc;
-    }, {});
-
-    setActivePersonaButton(target);
-    setInactiveSections(exclude);
-    setActiveSections(include);
-    setExpandedActivities([Object.keys(include)[0]]);
-  };
-
-  type ToggleFunction = (sectionTitle: string) => void;
-
-  const handleToggle: ToggleFunction = (sectionTitle) => {
-    console.log(sectionTitle);
-    setExpandedActivities(prevArray => {
-      const index = prevArray.indexOf(sectionTitle);
-      if (index !== -1) {
-        const newArray = [...prevArray.slice(0, index), ...prevArray.slice(index + 1)];
-        return newArray;
-      } else {
-        return [...prevArray, sectionTitle];
-      }
-    });
-  };
-
-  function getDefaultActiveState() {
-    return {
-      programming: 0,
-      backend: 1,
-      web: 2,
-      database: 3,
-      cloud: 4,
-      devops: 5,
-    };
-  }
-
-  function getDefaultInactiveState() {
-    return {
-      UX: 0,
-      "data visualization": 1,
-      "big data": 2,
-      "machine learning": 3,
-      leadership: 4,
-      "soft skills": 5,
-    };
-  }
-
-  const handleReset = () => {
-    setRotation(rotation + 360);
-    setActiveSections(getDefaultActiveState());
-    setInactiveSections(getDefaultInactiveState());
-    setActivePersonaButton("full");
-    setCurrentPersona("Full-Stack Developer")
-    setExpandedActivities([Object.keys(getDefaultActiveState())[0]]);
-  };
+  const descriptionMap: Map<string, string> = new Map([
+    ["Full-Stack Developer", "I build web applications from front to back, handling both client-side and server-side development."],
+    ["Frontend Developer", "I focus on crafting engaging user interfaces and ensuring seamless user experiences."],
+    ["Designer", "I create visually appealing designs with a focus on simplicity and intuitive interaction."],
+    ["Data Analyst", "I analyze data to derive insights and make informed decisions."],
+    ["Manager", "I lead teams and projects, ensuring efficient collaboration and successful outcomes."],
+    ["All", "Versatile Technologist."],
+  ]);
 
   return (
     <section id="skills" className={styles.skillsContainer}>
@@ -202,20 +120,21 @@ export default function Skills() {
 
         <div className={styles.personaExperienceContainer}>
           <div className={styles.personaContainer}>
-            <p className={styles.identifier}>Many Key Roles</p>
+            <p className={styles.identifier}>Key Roles</p>
             <div className={styles.buttonList}>
               {Array.from(experienceMap.keys()).map((persona: string, index: number) => (
+                <>
                 <button key={index} className={`${styles.skillButton} ${currentPersona === persona ? styles.selected : ''}`} onClick={() => setCurrentPersona(persona)}>
                   {currentPersona === persona && (
                     <FontAwesomeIcon icon={faCheck} />
                   )}
                   {persona}
                 </button>
+                {currentPersona === persona && <p className={styles.skillButtonDescription}>{descriptionMap.get(currentPersona)}</p>}
+                </>
               ))}
             </div>
           </div>
-
-          {/* <hr className={styles.buttonSectionSplit} /> */}
 
           <div className={styles.experienceContainer}>
             <p className={styles.identifier}>Highly Experienced In</p>
@@ -232,17 +151,7 @@ export default function Skills() {
 
         <div className={styles.activityCotainer}>
           {experienceMap.get(currentPersona)!.map((experience: Experience, index: number) => (
-            <CollapsibleSection
-              title={experience.longName}
-              isExpanded={expandedActivities.includes("programming")}
-              onToggle={() => handleToggle("programming")}
-            >
-              <ul key={index} className={styles.skillActivityList}>
-                {experience.skills.map((skill, skillIndex) => (
-                  <li key={skillIndex}>{skill}</li>
-                ))}
-              </ul>
-            </CollapsibleSection>
+            <p key={index}><b>{experience.longName}:</b> {experience.skills.join(", ")}</p>
           ))}
         </div>
       </div>
